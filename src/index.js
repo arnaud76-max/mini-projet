153518bffdb304d02bc2218b9f8451fa3d3b366f
arnaud76-max <72136722+arnaud76-max@users.noreply.github.com>
2021-04-21 00:54:52 +0200

    Update Metronome.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
 import React from "react";
+import ReactDom from "react-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Metronome from "./Metronome/Metronome";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+ReactDom.render(<Metronome  />, document.querySelector("root"));
