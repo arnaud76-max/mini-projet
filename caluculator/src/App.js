207c79fc72ebf072d02872f3a5d8b9bc2512d6df
arnaud76-max <72136722+arnaud76-max@users.noreply.github.com>
2021-04-28 20:14:09 +0200

    Create App.js

---
+++ caluculator/src/App.js
@@ -0,0 +1,77 @@
+
+import KeyPadComponent from "./component/KeyPadComponent";
+import "./App.css";
+import React, { Component } from 'react';
+import ResultComponent from './component/ResultComponent';
+
+
+class App extends Component {
+  constructor(){
+      super();
+
+      this.state = {
+          result: ""
+      }
+  }
+
+  onClick = button => {
+
+      if(button === "="){
+          this.calculate()
+      }
+
+      else if(button === "C"){
+          this.reset()
+      }
+      else if(button === "CE"){
+          this.backspace()
+      }
+
+      else {
+          this.setState({
+              result: this.state.result + button
+          })
+      }
+  };
+
+
+  calculate = () => {
+      try {
+          this.setState({
+              // eslint-disable-next-line
+              result: (eval(this.state.result) || "" ) + ""
+          })
+      } catch (e) {
+          this.setState({
+              result: "error"
+          })
+
+      }
+  };
+
+  reset = () => {
+      this.setState({
+          result: ""
+      })
+  };
+
+  backspace = () => {
+      this.setState({
+          result: this.state.result.slice(0, -1)
+      })
+  };
+
+  render() {
+      return (
+          <div>
+              <div className="calculator-body">
+                  <h1> Calculatrice NWS</h1>
+                  <ResultComponent result={this.state.result}/>
+                  <KeyPadComponent onClick={this.onClick}/>
+              </div>
+          </div>
+      );
+  }
+}
+
+export default App;
